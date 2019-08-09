a30e0af75fbaab89301e85d8e24085893278d654
jigracia <jigracia@miuandes.cl>
2019-08-09 09:43:14 -0400

    Fourth snap

--- Laboratorio01/Laboratorio01/Card/Game.cs
+++ Laboratorio01/Laboratorio01/Card/Game.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Laboratorio01.Card
 {
-    class Game
+    public class Game
     {
+        private Player players;
+        private Board boardGame;
+        private Player activePlayer;
+        private bool endGame;
+
+        public Player Players { get => players; set => players = value; }
+        public Board BoardGame { get => boardGame; set => boardGame = value; }
+        public Player ActivePlayer { get => activePlayer; set => activePlayer = value; }
+        public bool EndGame { get => endGame; set => endGame = value; }
+
+        public bool CheckIfEndGame() {
+            return endGame;
+        }
+
+        public int GetWinner() {
+            return players.PlayerID;
+        }
+
+        public void Play() {
+
+        }
     }
 }

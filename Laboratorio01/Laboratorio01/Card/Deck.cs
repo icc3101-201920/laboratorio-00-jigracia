b28ffc7525073fb506f992e9cfa9e695e053003a
jigracia <jigracia@miuandes.cl>
2019-08-09 09:33:53 -0400

    Third snap

---
+++ Laboratorio01/Laboratorio01/Card/Deck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio01.Card
+{
+    public class Deck
+    {
+        private List<CombatCard> combatCards;
+        private List<SpecialCard> specialCards;
+
+        public List<CombatCard> CombatCards { get => combatCards; set => combatCards = value; }
+        public List<SpecialCard> SpecialCards { get => specialCards; set => specialCards = value; }
+
+        public void AddCombatCard(CombatCard combatCard) {
+
+        }
+        public void AddSpecialCard(SpecialCard specialCard)
+        {
+
+        }
+
+        public void DestroyCombatCard(int cardID) {
+
+        }
+
+        public void DestroySpecialCards(int cardID) {
+
+        }
+
+        public void shuffle() {
+
+        }
+
+    }
+}
---
+++ Laboratorio01/Laboratorio01/Card/Hand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio01.Card
+{
+    public class Hand
+    {
+        private List<CombatCard> combatCards;
+        private List<SpecialCard> specialCards;
+
+        public List<CombatCard> CombatCards { get => combatCards; set => combatCards = value; }
+        public List<SpecialCard> SpecialCards { get => specialCards; set => specialCards = value; }
+
+        public void AddCombatCard(CombatCard combatCard) {
+
+        }
+        public void AddSpecialCards(SpecialCard specialCard) {
+
+        }
+        public void DestroyCombatCard(int cardID) {
+
+        }
+        public void SpecialCard(int cardID) {
+
+        }
+    }
+}

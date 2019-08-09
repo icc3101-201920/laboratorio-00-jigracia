ec8a50f05c61a6d4657010f710b967125ac2e360
jigracia <jigracia@miuandes.cl>
2019-08-09 09:01:37 -0400

    Second snap

---
+++ Laboratorio01/Laboratorio01/Card/Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio01.Card
+{
+    public class Board
+    {
+        private List<CombatCard> meleeCards;
+        private List<CombatCard> rangeCards;
+        private List<CombatCard> longRangeCards;
+        private SpecialCard specialMeleeCards;
+        private SpecialCard specialRangeCards;
+        private SpecialCard specialLongRangeCards;
+        private List<SpecialCard> captainCards;
+        private List<SpecialCard> weatherCards;
+
+        public List<CombatCard> MeleeCards { get => meleeCards; }
+        public List<CombatCard> RangeCards { get => rangeCards; }
+        public List<CombatCard> LongRangeCards { get => longRangeCards; }
+        public SpecialCard SpecialMeleeCards { get => specialMeleeCards; }
+        public SpecialCard SpecialRangeCards { get => specialRangeCards; }
+        public SpecialCard SpecialLongRangeCards { get => specialLongRangeCards; }
+        public List<SpecialCard> CaptainCards { get => captainCards; }
+        public List<SpecialCard> WeatherCards { get => weatherCards; }
+    }
+}
---
+++ Laboratorio01/Laboratorio01/Card/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio01.Card
+{
+    public class Player
+    {
+        private int lifPoints;
+        private int attackPoints;
+        private Deck deck;
+        private Hand hand;
+        private Board board;
+        private SpecialCard captain;
+
+        public int LifPoints { get => lifPoints; set => lifPoints = value; }
+        public int AttackPoints { get => attackPoints; set => attackPoints = value; }
+        public Deck Deck { get => deck; set => deck = value; }
+        public Hand Hand { get => hand; set => hand = value; }
+        public Board Board { get => board; set => board = value; }
+        public SpecialCard Captain { get => captain; set => captain = value; }
+
+        public void DrawCard() {
+
+        }
+        public void PlayCard(int cardID) {
+
+        }
+        public void ChangeCard(int cardID) {
+
+        }
+        public void FirstHand() {
+
+        }
+        public void ChooseCaptainCard(SpecialCard captainCard) {
+
+        }
+
+    }
+}
